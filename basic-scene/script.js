a5fa9b42b9f4ed5135adb766b7ef701c4c3a5983
JuCod <julien.coderch@gmail.com>
2023-04-01 00:40:13 +0200

    basics

---
+++ basic-scene/script.js
@@ -0,0 +1,29 @@
+// Description: Basic scene with a red cube
+const scene = new THREE.Scene();
+
+// Create a red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+// Add material to the cube
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+//Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Create a camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height );
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
